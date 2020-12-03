1e0ad46603ef71df4e3eeb92b306d4144e703baa
Haudy <haudy@neugelb.com>
2020-12-03 17:50:07 +0100

    fix layout

--- problem/android/filter/src/main/java/com/tokopedia/filter/view/ProductCardView.kt
+++ problem/android/filter/src/main/java/com/tokopedia/filter/view/ProductCardView.kt
@@ -6,6 +6,8 @@ import com.bumptech.glide.Glide
 import com.tokopedia.filter.R
 import com.tokopedia.filter.model.Product
 import kotlinx.android.synthetic.main.product_card.view.*
+import java.text.NumberFormat
+import java.util.*
 
 class ProductCardView(
         product: Product,
@@ -15,7 +17,7 @@ class ProductCardView(
     init {
         val view = inflate(context, R.layout.product_card, this)
         view.product_card_title.text = product.name
-        view.product_card_price.text = product.priceInt.toString()
+        view.product_card_price.text = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(product.priceInt.toDouble())
         view.product_card_city.text = product.shop.city
         view.product_card_shop.text = product.shop.name
         isClickable = true
